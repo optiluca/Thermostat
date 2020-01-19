04c2d2ab9758c74e9d7e4eb3ea65171ecce09e7b
Luca Bortolotti <optiluca@gmail.com>
2020-01-19 10:50:31 +0100

    Attempted to add logging (just writes empty files atm).  Set a very sad 15 degrees temperature across the board for now.

--- thermostat.py
+++ thermostat.py
@@ -1,12 +1,15 @@
 import datetime
+import logging
 import sys
 import time
+from logging.handlers import TimedRotatingFileHandler
+from logging import FileHandler
 
 from utils import set_boiler, read_temp
 
 NIGHT_TEMP = 15
-DAY_TEMP = 18
-AWAY_TEMP = 13
+DAY_TEMP = 15
+AWAY_TEMP = 15
 
 HYST = 1
 
@@ -47,23 +50,47 @@ def main_loop():
         t_current = read_temp()
         t_target = get_target_temp()
 
-        print('Target T: {}, Current T: {}'.format(t_target, t_current))
+        status_string = 'Target T: {}, Current T: {}'.format(t_target, t_current)
+        print(status_string)
+        logging.info(status_string)
 
         if t_current > t_target + HYST:
-            set_boiler(False)
+            boiler_state = False
         elif t_current < t_target - HYST:
-            set_boiler(True)
+            boiler_state = True
+        else:
+            boiler_state = None
+
+        if boiler_state is not None:
+            set_boiler(boiler_state)
+            logging.info('Boiler state: {}'.format(boiler_state))
+
         time.sleep(30)
+
+
+def setup_logger(logger_name='SimpleThermostatLogger', log_file='Thermostat.log', level=logging.INFO):
+    # Configure log file
+    l = logging.getLogger(logger_name)
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+
+    formatter = logging.Formatter(log_format)
+    # file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, delay=False)
+    file_handler = FileHandler(log_file, mode='w')
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    l.setLevel(level)
+    l.addHandler(file_handler)
+    l.addHandler(stream_handler)
 
 
 if __name__ == "__main__":
     print('Starting...')
+
+    setup_logger()
+
     try:
         main_loop()
     except KeyboardInterrupt:
-        # turn the relay off
         set_boiler(False)
-        print("\nExiting application\n")
-        time.sleep(3)
-        # exit the application
         sys.exit(0)
