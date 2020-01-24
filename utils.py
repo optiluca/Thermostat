b9788934d5ee528d484dddf03ecee344979569dc
Fifth <alexisgoodenough@gmail.com>
2020-01-24 22:56:47 +0100

    rename etc

--- utils.py
+++ utils.py
@@ -1,44 +1,19 @@
-import glob
-import time
-
-import gpiozero
+import datetime
 import yaml
 
-RELAY_PIN = 18
 
-relay = gpiozero.OutputDevice(RELAY_PIN, active_high=True, initial_value=False)
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def toggle_boiler():
-    print('Toggling relay...')
-    relay.toggle()
-
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
-
-
-def read_config(file_path):
+def try_read_config(file_path):
+    config = None
     with open(file_path, 'r') as stream:
         try:
             config = yaml.safe_load(stream)
+            isSuccess = True
         except yaml.YAMLError as exc:
             print(exc)
     return config
+
+
+def to_time(t):
+    if isinstance(t, int) or isinstance(t, float):
+        t = str(int(t)).zfill(4)
+    return datetime.datetime.strptime(t, '%H%M').time()
