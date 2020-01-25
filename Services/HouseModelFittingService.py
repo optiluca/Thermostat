5bf6401c7c5a516cdc9af397ebd49afd69f4a0b6
Fifth <alexisgoodenough@gmail.com>
2020-01-25 18:22:32 +0100

    fitting skeleton

--- Services/HouseModelFittingService.py
+++ Services/HouseModelFittingService.py
@@ -10,7 +10,6 @@ class HouseModelFittingService:
         self.house_database_service = HouseDatabaseService(False)
         self.thermostat_database_service = ThermostatDatabaseService(True)
 
-        self.is_best_fit_updated = False
         self.k1_best_fit = None
         self.k2_best_fit = None
 
@@ -26,7 +25,7 @@ class HouseModelFittingService:
             is_k1_latest_fit = False
             text_body = 'Best Fit K1: {}, Best Fit K2: {}, Latest Fit K2: {}'
 
-        if self.is_best_fit_updated and self.k1_best_fit and self.k2_best_fit:
+        if self.k1_best_fit and self.k2_best_fit:
             house_model = House(self.k1_best_fit, self.k2_best_fit)
 
             status_string = text_body.format(house_model.k1, house_model.k2, k)
@@ -37,15 +36,21 @@ class HouseModelFittingService:
         return house_model
 
     def _update_k1_best_fit(self):
-        # read data from database
-        # fit latest K1
-        # update best K1
-        self.is_best_fit_updated = False
-        return 0
+        new_k = self._fit_k1()
+        self.k1_best_fit = self._update_best_fit(new_k, self.k1_best_fit)
+        return new_k
 
     def _update_k2_best_fit(self):
-        # read data from database
-        # fit latest K2 given best K1
-        # update best K2
-        self.is_best_fit_updated = False
+        new_k = self._fit_k2()
+        self.k2_best_fit = self._update_best_fit(new_k, self.k2_best_fit)
+        return new_k
+
+    def _fit_k1(self):
         return 0
+
+    def _fit_k2(self):
+        return 0
+
+    def _update_best_fit(self, latest_fit, current_best_fit):
+        # e.g. confidence weighted average
+        return current_best_fit
